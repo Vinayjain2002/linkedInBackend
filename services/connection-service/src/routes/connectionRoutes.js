6be810d45ec4da9ddea46065fd77693c60087300
Vinay Jain <vinayjain61787@gmail.com>
2025-06-26 08:59:58 +0530

    Modified

--- services/connection-service/src/routes/connectionRoutes.js
+++ services/connection-service/src/routes/connectionRoutes.js
@@ -6,6 +6,7 @@ const connectionController = require('../controllers/connectionController.js');
 router.post('/request', auth, connectionController.sendRequest);
 router.post('/:connectionId/accept', auth, connectionController.acceptRequest);
 router.post('/:connectionId/reject', auth, connectionController.rejectRequest);
+
 router.get('/', auth, connectionController.listConnections);
 router.get('/pending', auth, connectionController.listPending);
 
